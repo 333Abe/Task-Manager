73452a168a04799290966578db657547c0d329dc
Adrian Bennett <adrianbennett333@gmail.com>
2024-10-04 11:04:55 +0100

    Updated to include task prioritisation levels(low, medium, high)

--- main.py
+++ main.py
@@ -7,13 +7,34 @@ def assign_id():
     except ValueError:
         return 1
 
-def add_task(desc):
+def add_task(desc, priority):
     id = assign_id()
+    match priority:
+        case 'h':
+            priority = 'high'
+        case 'm':
+            priority ='medium'
     task_list.append({
         'id': id,
         'desc': desc,
-        'status': 'incomplete'
+        'status': 'incomplete',
+        'priority': priority.title()
     })
+
+def print_tasks(status):
+    print(
+        f"{status.title()} tasks\n"
+        f"----------------"
+    )
+    for task in task_list:
+        if task['status'] == status:
+            print(
+                f"ID: {task['id']}\n"
+                f"Description:\n{task['desc']}\n"
+                f"Priority: {task['priority']}"
+                f"\n"
+            )
+
 
 def list_tasks():
 
@@ -25,13 +46,7 @@ def list_tasks():
         f"----------------"
     )
     if num_incomplete > 0:
-        for task in task_list:
-            if task['status'] == 'incomplete':
-                print(
-                    f"ID: {task['id']}\n"
-                    f"Description:\n{task['desc']}"
-                    f"\n"
-                )
+        print_tasks('incomplete')
     else:
         print("Empty")
 
@@ -41,13 +56,7 @@ def list_tasks():
         f"---------------"
     )
     if num_complete > 0:
-        for task in task_list:
-            if task['status'] == 'complete':
-                print(
-                    f"ID: {task['id']}\n"
-                    f"Description:\n{task['desc']}"
-                    f"\n"
-                )
+        print_tasks('complete')
     else:
         print("Empty")
 
@@ -118,7 +127,13 @@ while True:
                 f"Write a task description\n"
                 f">>> "
             )
-            add_task(desc)
+            priority = input(
+                f"Set priority: high, medium, low (defaults to low)\n"
+                f">>> "
+            )
+            if priority.lower() not in ['high', 'medium', 'h', 'm']:
+                priority = 'low'
+            add_task(desc, priority.lower())
             save_task_list()
         case 'complete' | 'c':
             task_id = input(
