c2a5ba093d806b835f821fc098250dd38aad5a1c
Adrian <adrianbennett333@gmail.com>
2025-01-05 17:37:42 +0000

    Refactor: Add type hints to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from help_info import show_help
 from task_manager import TaskManager
+from task import Task
+from commands import Command
 from commands import AddTaskCommand, DeleteTaskCommand, CompleteTaskCommand, ListTasksByPriorityCommand, ListTasksByStatusCommand, ModifyTaskPriorityCommand, ModifyTaskDescriptionCommand
 
-PRIORITY_MAP = {
+PRIORITY_MAP: dict[str | int] = {
     'h': 1,
     'high': 1,
     'm': 2,
@@ -11,22 +13,22 @@ PRIORITY_MAP = {
     'low': 3
 }
     
-def validate_id(task_id):
+def validate_id(task_id: str) -> int | bool:
     try:
-        task_id = int(task_id)
+        task_id: int = int(task_id)
         if task_id <= 0:
             raise ValueError
         return task_id
     except ValueError:
         return False
 
-def print_commands():
+def print_commands() -> None:
     print(f"Commands: list, add, complete, delete, modify, order, quit, help\n")
 
-def split_user_input(user_input):
-    command_list = []
-    command = ""
-    str_char = False
+def split_user_input(user_input: str) -> list[str]:
+    command_list: list = []
+    command: str = ""
+    str_char: bool | str = False
     for char in user_input:
         if char == " " and str_char == False:
             command_list.append(command)
@@ -45,19 +47,19 @@ def split_user_input(user_input):
     command_list.append(command)
     return command_list
 
-def return_task(task_id, task_manager):
-    task_id = validate_id(task_id)
+def return_task(task_id: str, task_manager: TaskManager) -> Task | bool:
+    task_id: int | bool = validate_id(task_id)
     if not task_id:
         print("Invalid id. Type 'help for more information.")
         return False
-    task = task_manager.get_task_by_id(task_id)
+    task: Task | None = task_manager.get_task_by_id(task_id)
     if not task:
         print("No task matches supplied id. Type 'help' for more information.")
         return False
     return task
 
-def input_parser(user_input, task_manager):
-    command = split_user_input(user_input)
+def input_parser(user_input: str, task_manager: TaskManager) -> bool:
+    command: list[str] = split_user_input(user_input)
     
     if command[0] not in ['list', 'l', 'lp', 'ls', 'add', 'a', 'complete', 'c', 'modify', 'm', 'delete', 'd', 'quit', 'q', 'help', 'h']:
         print("Unrecognised command. Type 'help' for more information.")
@@ -66,30 +68,31 @@ def input_parser(user_input, task_manager):
     if command[0] == 'list' or command[0] == 'l':
         if len(command) == 2:
             if command[1] == 's' or command[1] == 'status':
-                list_command = ListTasksByStatusCommand(task_manager)
+                list_command: Command = ListTasksByStatusCommand(task_manager)
                 list_command.execute()
                 return True
             if command[1] == 'p' or command[1] == 'priority':
-                list_command = ListTasksByPriorityCommand(task_manager)
+                list_command: Command = ListTasksByPriorityCommand(task_manager)
                 list_command.execute()
                 return True
         print("Invalid option(s) for 'list' function. Type 'help' for more information.")
         return True
         
     if command[0] == 'ls':
-        list_command = ListTasksByStatusCommand(task_manager)
+        list_command: Command = ListTasksByStatusCommand(task_manager)
         list_command.execute()
         return True
+    
     if command[0] == 'lp':
-        list_command = ListTasksByPriorityCommand(task_manager)
+        list_command: Command = ListTasksByPriorityCommand(task_manager)
         list_command.execute()
         return True
 
     if command[0] == 'add' or command[0] == 'a':
         if len(command) == 3 and command[2] in PRIORITY_MAP.keys():
-            desc = command[1]
-            priority = PRIORITY_MAP[command[2]]
-            add_command = AddTaskCommand(task_manager, desc, priority)
+            desc: str = command[1]
+            priority: int = PRIORITY_MAP[command[2]]
+            add_command: Command = AddTaskCommand(task_manager, desc, priority)
             add_command.execute()
             return True
         print("Invalid option(s) for 'add task' function. Type 'help' for more information.")
@@ -97,11 +100,11 @@ def input_parser(user_input, task_manager):
     
     if command[0] == 'complete' or command[0] == 'c':
         if len(command) == 2:
-            task_id = command[1]
-            task = return_task(task_id, task_manager)
+            task_id: str = command[1]
+            task: Task | bool = return_task(task_id, task_manager)
             if not task:
                 return True
-            complete_command = CompleteTaskCommand(task_manager, task)
+            complete_command: Command = CompleteTaskCommand(task_manager, task)
             complete_command.execute()
             return True
         print("Invalid option(s) for 'complete task' function. Type 'help' for more information.")
@@ -109,18 +112,18 @@ def input_parser(user_input, task_manager):
     
     if command[0] == 'modify' or command[0] == 'm':
         if len(command) == 4:
-            task_id = command[1]
-            task = return_task(task_id, task_manager)
+            task_id: str = command[1]
+            task: Task | bool = return_task(task_id, task_manager)
             if not task:
                 return True
             if command[2] == 'd':
-                desc = command[3]
-                mod_desc_command = ModifyTaskDescriptionCommand(task_manager, task, desc)
+                desc: str = command[3]
+                mod_desc_command: Command = ModifyTaskDescriptionCommand(task_manager, task, desc)
                 mod_desc_command.execute()
                 return True
             if command[2] == 'p' and command[3] in PRIORITY_MAP.keys():
-                priority = PRIORITY_MAP[command[3]]
-                mod_priority_command = ModifyTaskPriorityCommand(task_manager, task, priority)
+                priority: int = PRIORITY_MAP[command[3]]
+                mod_priority_command: Command = ModifyTaskPriorityCommand(task_manager, task, priority)
                 mod_priority_command.execute()
                 return True
         print("Invalid option(s) for 'modify task' function. Type 'help' for more information.")
@@ -128,18 +131,18 @@ def input_parser(user_input, task_manager):
     
     if command[0] == 'delete' or command[0] == 'd':
         if len(command) == 2:
-            task_id = command[1]
-            task = return_task(task_id, task_manager)
+            task_id: str = command[1]
+            task: Task | bool = return_task(task_id, task_manager)
             if not task:
                 return True
             task_manager.print_single_task(task)
-            confirm = input(
+            confirm: str = input(
                 f"\n"
                 f"Confirm deletion: Y/N\n"
                 f">>> "
             )
             if confirm.lower() in ['y','yes']:
-                delete_command = DeleteTaskCommand(task_manager, task)
+                delete_command: Command = DeleteTaskCommand(task_manager, task)
                 delete_command.execute()
             return True
         print("Invalid option(s) for 'delete task' function. Type 'help' for more information.")
@@ -151,8 +154,8 @@ def input_parser(user_input, task_manager):
     if command[0] == 'quit' or command[0] == 'q':
         return False
     
-def main():
-    task_manager = TaskManager()
+def main() -> None:
+    task_manager: TaskManager = TaskManager()
     print(task_manager.load_task_list())
     operate = True
 
@@ -160,8 +163,8 @@ def main():
     print_commands()
 
     while True:
-        user_input = input(">>> ")
-        operate = input_parser(user_input, task_manager)
+        user_input: str = input(">>> ")
+        operate: bool = input_parser(user_input, task_manager)
         task_manager.save_task_list()
         if operate == False:
             break
